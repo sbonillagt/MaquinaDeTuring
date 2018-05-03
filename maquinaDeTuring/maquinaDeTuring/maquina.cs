ca9f31e89b3540eba245998c202a808958ed5ce7
Sebastian Bonilla <sbonilla@correo.url.edu.gt>
2018-05-02 19:17:44 -0600

    Estructura de Form

---
+++ maquinaDeTuring/maquinaDeTuring/maquina.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maquinaDeTuring
+{
+    class maquina
+    {
+        Dictionary<string, string> palindromosDiccionarioEstados = new Dictionary<string, string>();
+        Dictionary<string, string> copiaPatronesDiccionarioEstados = new Dictionary<string, string>();
+        Dictionary<string, string> multiplicacionDiccionarioEstados = new Dictionary<string, string>();
+        Dictionary<string, string> sumaDiccionarioEstados = new Dictionary<string, string>();
+        Dictionary<string, string> restaDiccionarioEstados = new Dictionary<string, string>();
+
+
+        //diccionarios
+        //diccionario suma
+        public void agregarDiccionarios() {
+            palindromosDiccionarioEstados.Add("q0,1","1,1,R");
+            palindromosDiccionarioEstados.Add("q0,+", "+,1,R");
+            palindromosDiccionarioEstados.Add("q1,1", "1,1,R");
+            palindromosDiccionarioEstados.Add("q1,+", "B,B,L");
+            palindromosDiccionarioEstados.Add("q2,1", "1,B,R");
+
+        }
+    }
+}
